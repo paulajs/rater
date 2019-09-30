e2992c779bc6b0446362a12f2b418aa4a9b7fb64
Ronni Skansing <rskansing@gmail.com>
2019-09-30 20:55:33 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App: React.FC = () => {
+import './App.css';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
+import { Create } from './pages/Create';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header></Header>
+       <div>
+         <Route path="/" exact component={Home} />
+         <Route path="/create" component={Create} />
+       </div>
+      </Router>
     </div>
   );
 }
